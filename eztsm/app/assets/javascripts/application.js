a06d0d49eed577057f9ae141f8a6f1a533781004
Loic Basset <lowouik@gmail.com>
2016-01-26 13:19:31 +1100

    Add client side validations js

--- eztsm/app/assets/javascripts/application.js
+++ eztsm/app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require rails.validations
 //= require_tree .
 
 $(document).on("ready page:change", function() {
