900a8d0cf18d98b06c02fd4c3df2063d567f545e
Loic Basset <lowouik@gmail.com>
2016-01-28 11:16:09 +1100

    Starting nodes controller creation

--- eztsm/app/controllers/nodes_controller.rb
+++ eztsm/app/controllers/nodes_controller.rb
@@ -1,2 +1,13 @@
 class NodesController < ApplicationController
+
+  def index
+    if Setting.local_tsm
+      @nodes = `ls` 
+    else
+#      @nodes = `ssh -p #{Setting.ssh_port} #{Setting.ssh_user}@#{Setting.tsm_address} ls`
+      @nodes = `echo ssh -p #{Setting.ssh_port} #{Setting.ssh_user}@#{Setting.tsm_address} ls`
+      puts @nodes
+      render :nothing => true
+    end
+  end
 end
