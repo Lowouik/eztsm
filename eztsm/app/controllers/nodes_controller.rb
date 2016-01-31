a00cfdaa2919593c9865950e7e4701c42e2428ed
Loic Basset <lowouik@gmail.com>
2016-02-01 08:39:25 +1100

    Nodes search

--- eztsm/app/controllers/nodes_controller.rb
+++ eztsm/app/controllers/nodes_controller.rb
@@ -2,8 +2,8 @@ class NodesController < ApplicationController
 
   def index
     @node_name = params[:node_name]
-    @filter = @node_name.upcase.gsub(/[^0-9A-Z\-\.]/i, '')
-    @columns = [ 'node_name', 'platform_name', 'domain_name', 'option_set', 'lastacc_time']
+    @filter = @node_name.upcase.gsub(/\*/i, '%').gsub(/[^0-9A-Z\-\.%]/i, '')
+    @columns = [ 'node_name', 'platform_name', 'domain_name', 'option_set', 'timestamp(lastacc_time, 0)']
     @select = tsmdb_select(@columns, 'nodes', "node_name like \'%#{@filter}%\'")
     @nodes = @select['output'].split(/\n/)
   end
