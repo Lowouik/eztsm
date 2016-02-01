27249cf9ab2ceba6b41f64c98537d48a7c32915b
Loic Basset <lowouik@gmail.com>
2016-02-02 09:34:38 +1100

    Rename node part #1

--- eztsm/app/assets/javascripts/nodes.js
+++ eztsm/app/assets/javascripts/nodes.js
@@ -15,6 +15,7 @@ function getNodeNameFromTable(row){
     url: '/node?node_name=' + nodeName,
     data: node,
     success: function(node){
+      // Fetch node properties then display them
       for (key in node[0]){
         if (key == 'lastacc_time' || key == 'max_mp_allowed') {
           $('#viewNodeModalBodyMain').append('<hr />')
@@ -31,6 +32,11 @@ function getNodeNameFromTable(row){
           '</row>'
         )
       }
+      // Customize some fields that can be modified through ajax requests
+      // Let's start with the node name
+      renameNodeButton = ' <button type="button" class="btn btn-xs btn-default has-tooltip" title="Rename" onclick="renameNode(\'' + node[0]['node_name'] + '\')"><span class="glyphicon glyphicon-option-horizontal"></span></button>'
+      $('#viewNodeModalBodyMain-node_name').append(renameNodeButton) 
+
     },
     error: function() {
       $('#viewNodeModalBodyErrors').html('<span class="text-danger">An error occured while trying to retrieve node informations. Please try again later.</span>')
@@ -41,3 +47,11 @@ function getNodeNameFromTable(row){
   });
 }
 
+function renameNode(currentNode){
+  $('#viewNodeModalBodyMain-node_name').html('<input id="viewNodeModalBodyMain-node_name-inputField" class="string required" style="width:' + (currentNode.length + 1) * 9 + 'px" type="text" value="' + currentNode + '"/>' + renameNodeButton + ' <button class="btn btn-xs btn-success has-tooltip" title="OK" onclick="#"><span class="glyphicon glyphicon-ok-circle"></span></button> <button class="btn btn-xs btn-danger has-tooltip" title="Cancel" onclick="cancelNodeRenaming(\'' + currentNode + '\')"><span class="glyphicon glyphicon-remove-circle"></span></button>')
+}
+
+function cancelNodeRenaming(node){
+  $('#viewNodeModalBodyMain-node_name').html(node + renameNodeButton)
+  $('#viewNodeModalBodyErrors').html('')
+}
