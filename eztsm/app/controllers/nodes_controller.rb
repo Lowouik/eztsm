70b8e27fb885e1d036b5268df63a1fc11d376eb3
Loic Basset <lowouik@gmail.com>
2016-02-17 09:14:33 +1100

    IP is now displayed in nodes index instead of OS

--- eztsm/app/controllers/nodes_controller.rb
+++ eztsm/app/controllers/nodes_controller.rb
@@ -7,7 +7,7 @@ class NodesController < ApplicationController
       @node_name = ''
     end
     @filter = sanitize_for_tsm(@node_name).upcase
-    @columns = [ 'node_name', 'platform_name', 'domain_name', 'option_set', 'timestamp(lastacc_time, 0)']
+    @columns = [ 'node_name', 'tcp_address', 'domain_name', 'option_set', 'timestamp(lastacc_time, 0)']
     @select = tsmdb_select(@columns, 'nodes', "node_name like \'%#{@filter}%\'")
     @nodes = @select['output'].split(/\n/)
   end
