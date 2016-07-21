cf7d6d92d74ded725dd608c4bdf900df907a9db4
Loic Basset <lowouik@gmail.com>
2016-07-21 11:26:28 +1100

    User password must be 8 char long

--- eztsm/app/models/user.rb
+++ eztsm/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
     
   validates_presence_of :password_digest, :login
+  validates :password, presence: { on: create }, length: { minimum: 8 }, if: :password_digest_changed?
   validates :login, :uniqueness => { :case_sensitive => false }
   validates :email, :allow_blank => true, :uniqueness => { :case_sensitive => false }
 end
