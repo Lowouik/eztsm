ef7669842e32a5039e5a0419efed6bb1401d0005
Loic Basset <lowouik@gmail.com>
2016-07-22 15:05:40 +1100

    Paginate events

--- eztsm/app/controllers/admin/events_controller.rb
+++ eztsm/app/controllers/admin/events_controller.rb
@@ -1,5 +1,5 @@
 class Admin::EventsController < ApplicationController
   def index
-    @events = Event.order(timestamp: :desc)
+    @events = Event.paginate(:page => params[:page], :per_page => 20).order(timestamp: :desc)
   end
 end
