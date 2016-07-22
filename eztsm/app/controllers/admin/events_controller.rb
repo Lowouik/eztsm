4a564ff361fa2f1a16ee047844564a97772fd7f6
Loic Basset <lowouik@gmail.com>
2016-07-22 17:30:27 +1100

    Pagination and scopes (filters) for events display

--- eztsm/app/controllers/admin/events_controller.rb
+++ eztsm/app/controllers/admin/events_controller.rb
@@ -1,5 +1,13 @@
 class Admin::EventsController < ApplicationController
+  has_scope :by_user
+  has_scope :after
+  has_scope :before
+
   def index
-    @events = Event.paginate(:page => params[:page], :per_page => 20).order(timestamp: :desc)
+    @by_user = params[:by_user]
+    @after = params[:after]
+    @before = params[:before]
+    @events = apply_scopes(Event).all.paginate(:page => params[:page], :per_page => 20).order(timestamp: :desc)
+    #    @events = Event.paginate(:page => params[:page], :per_page => 20).order(timestamp: :desc)
   end
 end
--- eztsm/app/models/event.rb
+++ eztsm/app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  scope :by_user, lambda {|user_login| where('users.login = ?', user_login).joins(:user)}
+  scope :after, -> after { where("timestamp >= ?", after) }
+  scope :before, -> before { where("timestamp <= ?", before.to_date + 1.day) }
 end
